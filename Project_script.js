6689e21e1a91bb2519d7ba431229978effc6a0e8
abhilaash-arch <galiabhilaash@gmail.com>
2026-04-24 13:24:54 +0530

    Add files via upload

---
+++ Project_script.js
@@ -0,0 +1,28 @@
+function selectbutton(){
+    let science = false;
+    let sports = false;
+    let tech = false;
+    let maths = false;
+    let selected = false;
+    if (!science){
+        selected = true;
+        science = true;
+    }
+    else if(!sports){
+
+        
+        selected = true;
+        sports = true;
+    }
+    else if(!tech){
+        selected = true;
+        tech = true;
+    }
+    else if(!maths){
+        selected = true;
+        maths = true;
+    }
+}
+function start(){
+    
+}
